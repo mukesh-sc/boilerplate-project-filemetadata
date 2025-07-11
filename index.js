cfe06c69d31b57b8dbb74aaff896545d2f89f6ff
mukesh-sc <mukesh.scroffice@gmail.com>
2025-07-11 22:52:04 +0530

    Update index.js with done project 5

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
@@ -12,6 +13,19 @@ app.get('/', function (req, res) {
 });
 
 
+// multer setup 
+const upload = multer({dest:'uploads/'})
+
+app.post("/api/fileanalyse", upload.single('upfile'),(req, res)=>{
+  if(!req.file){
+    return res.status(400).json({error: "No file uploaded"});
+  }
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,  
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
